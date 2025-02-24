ea763c5e12c69e90b55a7ab6f82ac8b20b29943f
luceedot <asqv0001@stud.kea.dk>
2025-02-24 11:01:55 +0100

    test

---
+++ src/main/java/com/example/gitpractice240225/GitPractice240225Application.java
@@ -0,0 +1,13 @@
+package com.example.gitpractice240225;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractice240225Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPractice240225Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitpractice240225/GitPractice240225ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitpractice240225;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPractice240225ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
